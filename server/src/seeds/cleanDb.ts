090d3b0266482d64081f4d51ac88f11dd8bb973f
AttackOnBaron <gianni-2008@hotmail.com>
2024-12-08 02:22:49 -0600

    test push

--- server/src/seeds/cleanDb.ts
+++ server/src/seeds/cleanDb.ts
@@ -3,6 +3,9 @@ import db from '../config/connection.js';
 
 export default async (modelName: "Question", collectionName: string) => {
   try {
+    if (!models[modelName]) {
+      throw new Error(`Model ${modelName} does not exist in models/index.js`);
+    }
     let modelExists = await models[modelName].db.db.listCollections({
       name: collectionName
     }).toArray()
